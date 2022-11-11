d29f72345b67197ea423a033cc19cce2da79d5ca
Gianlucas <lukasameed12@gmail.com>
2022-11-11 01:42:15 -0500

    Se agrega el archivo para la conexion a la base de datos

---
+++ base_datos/conexion_sqlserver.py
@@ -0,0 +1,26 @@
+import pyodbc
+
+class BD():
+    contador_conexion = 0
+    conexion = None
+    cursor = None
+
+    def iniciar_conexion_db(self):
+        BD.contador_conexion += 1
+        if BD.contador_conexion == 1:
+            try:
+                BD.conexion = pyodbc.connect('DRIVER={ODBC DRIVER 17 for SQL Server};SERVER=(local);DATABASE=Emssamlud;UID=lucas;PWD=123')
+                print('Conexion exitosa')
+
+            except Exception as ex:
+                print(ex)
+
+            BD.cursor = BD.conexion.cursor()
+            print('Cursor creado')
+
+    def finalizar_conexion_db(self):
+        BD.cursor.close()
+        print('Cursor cerrado')
+        BD.conexion.close()
+        print('Conexion finalizada')
+
