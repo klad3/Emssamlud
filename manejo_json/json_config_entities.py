a92b85f66c3b3cb87f6134b986685e8c66834f56
klad3 <cjumojosecarlos@gmail.com>
2022-12-15 12:05:53 -0500

    Se hace mejoras en la interfaz JsonConfigEntities

--- manejo_json/json_config_entities.py
+++ manejo_json/json_config_entities.py
@@ -2,17 +2,17 @@ from abc import ABC, abstractmethod
 
 class JsonConfigEntities(ABC):
     @abstractmethod
-    def registrar_json(self):
+    def registrar_json(self, lista):
         pass
 
     @abstractmethod
-    def verificar_json(self):
+    def verificar_json(self, id):
         pass
 
     @abstractmethod
-    def modificar_json(self):
+    def modificar_json(self, **kwargs):
         pass
 
     @abstractmethod
-    def eliminar_json(self):
+    def eliminar_json(self, id):
         pass
