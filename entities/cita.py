1f3f50e1647f8ff5a663167a25383454d438eeba
Jorge Ipanaque <jorgep.ipanaquep@gmail.com>
2022-12-22 22:48:27 -0500

    Se eliminan archivos innecesarios y se modifican cita y médico para el correcto funcionamiento del programa.

--- entities/cita.py
+++ entities/cita.py
@@ -1,74 +1,31 @@
 class Cita:
-    def __init__(self, id, paciente, area, medico, fecha_cita) -> None:
-        self._id = id
-        self._fecha_cita = fecha_cita
-        self._area = area
-        self._medico = medico
-        self._paciente = paciente
+    def __init__(self, id, paciente, area, medico, fecha_cita):
+        self.__id = id
+        self.__paciente = paciente
+        self.__area = area
+        self.__medico = medico
+        self.__fecha_cita = fecha_cita
 
     @property
     def id(self):
         return self.__id
 
-    @id.setter
-    def fecha_cita(self, id):
-        self.__id = id
+    @property
+    def paciente(self):
+        return self.__paciente
 
     @property
     def fecha_cita(self):
         return self.__fecha_cita
 
-    @fecha_cita.setter
-    def fecha_cita(self, fecha_cita):
-        self.__fecha_cita = fecha_cita
-
-    @property
-    def hora(self):
-            return self.__hora
-
-    @hora.setter
-    def hora(self, hora):
-        self.__hora = hora
-
     @property
     def area(self):
         return self.__area
-
-    @area.setter
-    def area(self, area):
-        self.__area = area
 
     @property
     def medico(self):
         return self.__medico
 
-    @medico.setter
-    def medico(self, medico):
-        self.__medico = medico
-
-    @property
-    def consultorio(self):
-        return self.__consultorio
-
-    @consultorio.setter
-    def consultorio(self, consultorio):
-        self.__consultorio = consultorio
-
     @property
     def paciente_dni(self):
         return self.__paciente
-
-    @paciente_dni.setter
-    def paciente_dni(self, paciente):
-        self.__paciente = paciente
-
-
-
-    # Se agenda la cita
-    def agendar_cita(self, fecha_cita, precio, estado):
-        pass
-    # Se efectuara el pago de la cita agendada
-    
-    # Se archiva la cita
-    def archivar_cita(self, estado):
-        pass
--- entities/medico.py
+++ entities/medico.py
@@ -1,23 +1,48 @@
-from .personal_salud import Personal
-
-class Medico(Personal):
+class Medico:
     def __init__(self, dni, nombres, apellido_paterno, apellido_materno,
-                telefono, sexo, disponibilidad, ocupacion, especialidad, citas_disponibles):
-        super().__init__(dni, nombres, apellido_paterno, apellido_materno,
-                        telefono,sexo, disponibilidad, ocupacion)
+                telefono, sexo, disponibilidad, especialidad, citas_disponibles):
+        self.__dni = dni
+        self.__nombres = nombres
+        self.__apellido_paterno = apellido_paterno
+        self.__apellido_materno = apellido_materno
+        self.__telefono = telefono
+        self.__sexo = sexo
+        self.__disponibilidad = disponibilidad
         self.__especialidad = especialidad
         self.__citas_disponibles = citas_disponibles
 
     @property
+    def dni(self):
+        return self.__dni
+    
+    @property
+    def nombres(self):
+        return self.__nombres
+    
+    @property
+    def apellido_paterno(self):
+        return self.__apellido_paterno
+    
+    @property
+    def apellido_materno(self):
+        return self.__apellido_materno
+
+    @property
+    def telefono(self):
+        return self.__telefono
+    
+    @property
+    def sexo(self):
+        return self.__sexo
+
+    @property
+    def disponibilidad(self):
+        return self.__disponibilidad
+
+    @property
     def especialidad(self):
         return self.__especialidad
-
-    def atender(self, dni, disponibilidad):
-        pass
-
-    def diagnosticar(self, dni, diagnostico):
-        pass
-
-    def recetar(self, dni, medicamento):
-        pass
-
+    
+    @property
+    def citas_disponibles(self):
+        return self.__citas_disponibles
